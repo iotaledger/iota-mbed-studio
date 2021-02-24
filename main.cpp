f284c00746b83de49b52a3b36152781527191d98
Sam Chen <sam.chen@iota.org>
2021-02-24 15:17:14 +0800

    add mbed wifi lib

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 // main() runs in its own thread in the OS
 int main()
 {
+    printf("WiFi example\n");
+
+#ifdef MBED_MAJOR_VERSION
+    printf("Mbed OS version %d.%d.%d\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+#endif
+
+    printf("\nDone\n");
+
     while (true) {
 
     }
