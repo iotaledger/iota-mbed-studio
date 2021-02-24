ac819d9433f9a2fd91b095bc27d8540b293996a2
Sam Chen <sam.chen@iota.org>
2021-02-24 20:53:10 +0800

    add cjson and uthash libs
    Note: strdup is not standard API which it's added in utarray.h

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "mbed.h"
 
-#include "libhydrogen/hydrogen.h"
+#include "hydrogen.h"
+#include "cJSON.h"
+#include "utarray.h"
 
 // main() runs in its own thread in the OS
 int main()
@@ -10,6 +12,8 @@ int main()
 #ifdef MBED_MAJOR_VERSION
     printf("Mbed OS version %d.%d.%d\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
 #endif
+
+    // test hydrogen
     int ret = hydro_init();
     char buf[100] = {};
     printf("ret : %d\n", ret);
@@ -17,6 +21,31 @@ int main()
     for(size_t i = 0; i < 100; i++){
         printf("0x%x", buf[i]);
     }
+    printf("\n");
+
+    // test cjson 
+    cJSON *monitor = cJSON_CreateObject();
+    if (monitor == NULL){
+        printf("cjson create object failed\n");
+    }else{
+        cJSON_free(monitor);
+        printf("create and free cjson object done\n");
+    }
+
+    //test utarray
+    UT_array *nums;
+    int i, *p;
+    utarray_new(nums,&ut_int_icd);
+    for(i=0; i < 10; i++) utarray_push_back(nums,&i);
+
+    for(p=(int*)utarray_front(nums);
+        p!=NULL;
+        p=(int*)utarray_next(nums,p)) {
+        printf("%d",*p);
+    }
+    printf("\n");
+    utarray_free(nums);
+
     // printf("i32 : %d\n", (int)hydro_random_u32());
     printf("\nDone\n\n");
 
